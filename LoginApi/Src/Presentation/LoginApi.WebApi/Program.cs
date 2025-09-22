2bef06c450aa680e37b792a5f0257d184acd6893
aravind_git <stl1@hybrhind.com>
2025-09-22 17:39:24 +0530

    program

--- LoginApi/Src/Presentation/LoginApi.WebApi/Program.cs
+++ LoginApi/Src/Presentation/LoginApi.WebApi/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using FluentValidation.AspNetCore;
 using HyBrCRM.Application.Interfaces.Repositories;
@@ -126,6 +127,25 @@ public class Program
         builder.Services.AddSingleton(timeZone);
       
         var app = builder.Build();
+        var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+        app.MapGet("/weatherforecast", () =>
+        {
+            var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+                .ToArray();
+            return forecast;
+        })
+        .WithName("GetWeatherForecast");
+
         //app.UseExceptionHandler(a => a.Run(async context =>
         //{
         //    Console.WriteLine("exception");
@@ -176,5 +196,9 @@ public class Program
         app.MapRazorPages();
         app.MapGet("/", () => Results.Redirect("/Index"));
         await app.RunAsync();
+    }
+    internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+    {
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
     }
 }
